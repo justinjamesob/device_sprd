b07e99b371b8f6d828cf98455b347e0db08d56b0
juan.wu <juan.wu@spreadtrum.com>
2013-08-02 10:31:00 +0800

    Bug#194947 ap端的fixnv功能开发
    [bug number ] 194947
    [rootcause ]修改底层存储data结构为fixnv的结构
    [changes ]
    [side effects]
    [reviewers ]jason.wu
    
    Change-Id: I90c1f34e1f38cfbdb752bc7bfa87ea3cf056c023

--- common/apps/apnv/apnv_api.h
+++ common/apps/apnv/apnv_api.h
@@ -6,7 +6,5 @@ NVITEM_ERROR_E PRONV_WriteItem(uint32 nv_id,uint8 * buf,uint16 size);
 
 NVITEM_ERROR_E PRONV_ReadItem(uint32 nv_id,uint8 * buf,uint16 size);
 
-NVITEM_ERROR_E PRONV_DelItem(uint32 From,uint32 To);
-
 void productnv_unInit(void);
 
