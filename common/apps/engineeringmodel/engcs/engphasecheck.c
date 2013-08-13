1b1fb42202530fc7d4a3686606d849c3f5d9cdcc
Keny Liu(刘建峰) <keny.liu@spreadtrum.com>
2013-08-13 19:59:08 +0800

    BUG#201276 Get SN from Phase Check [bug number ] 201276 [root cause ] read error memory address [changes ]    common/apps/engineeringmodel/engcs/engphasecheck.h    common/apps/engineeringmodel/engcs/engphasecheck.c    sp8830eb/ueventd.sc8830.rc    sp8830ec/ueventd.sc8830.rc [side effects] none [reviewers ] jiayong.yang
    Change-Id: I369a92a454a3c83eec42a1b5123e3901c5903491

--- common/apps/engineeringmodel/engcs/engphasecheck.c
+++ common/apps/engineeringmodel/engcs/engphasecheck.c
@@ -5,6 +5,7 @@
 
 
 #define PHASE_CHECKE_FILE "/productinfo/productinfo.bin"
+#define PHASE_CHECKE_FILE0 "/dev/block/mmcblk0p13" //add by kenyliu on 2013 08 13 for read sn bug 201276
 
 int eng_phasechecktest(void)
 {
@@ -17,24 +18,52 @@ int eng_getphasecheck(SP09_PHASE_CHECK_T* phase_check)
 {
 	int ret = 0;
 	int len;
-	int fd = open(PHASE_CHECKE_FILE,O_RDONLY);
+	//modify  by kenyliu on 2013 08 13 for read sn bug 201276
+
+       SP09_PHASE_CHECK_T phase_check_temp;
+	int fd = open(PHASE_CHECKE_FILE0,O_RDONLY);
+	if(fd >= 0)
+	{
+		ENG_LOG("%s open Ok PHASE_CHECKE_FILE0 = %s ",__FUNCTION__ , PHASE_CHECKE_FILE0);
+		len = read(fd,&phase_check_temp,sizeof(SP09_PHASE_CHECK_T));
+
+		ENG_LOG("Magic0=%d",phase_check_temp.Magic);
+
+		ENG_LOG("SN01=%s",phase_check_temp.SN1);
+		ENG_LOG("SN02=%s",phase_check_temp.SN2);
+
+		close(fd);
+	}else{
+		ENG_LOG("%s open fail PHASE_CHECKE_FILE0 = %s ",__FUNCTION__ , PHASE_CHECKE_FILE0);
+	}
+
+	fd = open(PHASE_CHECKE_FILE,O_RDONLY);
 	if (fd >= 0){
+		ENG_LOG("%s open Ok PHASE_CHECKE_FILE = %s ",__FUNCTION__ , PHASE_CHECKE_FILE);
 		len = read(fd,phase_check,sizeof(SP09_PHASE_CHECK_T));
 
 		ENG_LOG("Magic=%d",phase_check->Magic);
 	
 		ENG_LOG("SN1=%s",phase_check->SN1);
 		ENG_LOG("SN2=%s",phase_check->SN2);
-	
+
+		if((phase_check_temp.Magic == SP09_SPPH_MAGIC_NUMBER) || (phase_check_temp.Magic == SP05_SPPH_MAGIC_NUMBER)){
+			strcpy(phase_check->SN1, phase_check_temp.SN1);
+			strcpy(phase_check->SN2, phase_check_temp.SN2);
+		}
+
+		ENG_LOG("SN1=%s",phase_check->SN1);
+		ENG_LOG("SN2=%s",phase_check->SN2);
+
 		if (len <= 0){
 			ret = 1;
 		}
 		close(fd);
 	} else {
-		ENG_LOG("%s open fail",__FUNCTION__);
+		ENG_LOG("%s open fail PHASE_CHECKE_FILE = %s ",__FUNCTION__ , PHASE_CHECKE_FILE);
 		ret = 1;
 	}
-
+//end modify kenyliu
 	return ret;
 }
 
