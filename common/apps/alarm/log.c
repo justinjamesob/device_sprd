ce0a6b37f3180184706a7722da94119e5cfc8d54
lisa.liu <lisa.liu@spreadtrum.com>
2013-09-09 11:04:45 +0800

    bug #210554 - coverity21571: unchecked return value
    [bug number ]210554
    [root cause ]without checking the return value of open & fcntl function
    [changes ]device/sprd/common/apps/alarm/log.c
    [side effects]
    [reviewers ]
    
    Change-Id: I67e26d66097d6d009f8a82d3946a83cf6744e803
    Signed-off-by: lisa.liu <lisa.liu@spreadtrum.com>

--- common/apps/alarm/log.c
+++ common/apps/alarm/log.c
@@ -17,9 +17,21 @@ static int log_fd = -1;
 void log_init(void)
 {
 	static const char *name = "/dev/__kmsg__";
+	int val;
 	if (mknod(name, S_IFCHR | 0600, (1 << 8) | 11) == 0) {
 		log_fd = open(name, O_WRONLY);
-		fcntl(log_fd, F_SETFD, FD_CLOEXEC);
+		if(log_fd < 0){
+			LOGE("%s: open file/dev/__kmsg__ fail.\n",__func__);
+			return;
+		}
+		val=fcntl(log_fd, F_SETFD, FD_CLOEXEC);
+		if (val == -1) {
+			LOGE("%s: file lock fails.\n",__func__);
+			close(log_fd);
+			log_fd =-1;
+			unlink(name);
+			return;
+	}
 		unlink(name);
 	}
 }
