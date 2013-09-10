916ef6b104b5a2fdbd8fe4880f7ec3e09dfa5aa6
liuyang <liuyang@spreadst.com>
2013-09-10 16:17:17 +0800

    bug#186376 InsertData tool
    [bug number  ]186376
    [root cause  ]
    [changes     ]
    [side effects]
    [reviewers   ]
    
    Change-Id: I3bd151e5db775ad2430ebbb8854b1097ad2b1ea2

---
+++ common/tools/InsertData/src/com/android/insertdata/addmms/MmsConfig.java
@@ -0,0 +1,14 @@
+package com.android.insertdata.addmms;
+
+public class MmsConfig {
+
+	public String phoneNo;
+	public boolean isAutoUp = false;
+	public int mmsCount;
+	public int mmsSize;
+	public String subject;
+	public boolean isRead = false;
+	public String sim = "0";
+	public int boxId;
+
+}
---
+++ common/tools/InsertData/src/com/android/insertdata/smstest/entity/SmsConfig.java
@@ -0,0 +1,15 @@
+package com.android.insertdata.smstest.entity;
+
+public class SmsConfig {
+	public String phoneNo;
+	public boolean isAutoUp = false;
+	public String smsContent;
+	public int count;
+	public int threadCount;
+	public String subject;
+	public boolean isRead = false;
+	public String sim = "0";
+	public boolean isThread = false;
+	public String threadId;
+	public static String boxId;
+}
