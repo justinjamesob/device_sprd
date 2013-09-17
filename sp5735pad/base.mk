1d1ef93303bb0402041447e7279c6e89bd9543bb
yunlong.shi <yunlong.shi@spreadtrum.com>
2013-09-17 21:26:20 +0800

    Bug#215718, shark projects enable refnotify
    Change-Id: I28db213e97d36f8b34bc7ddde5eea04b68c0a867

--- sp5735pad/base.mk
+++ sp5735pad/base.mk
@@ -74,6 +74,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	nvitemd \
 	charge \
+	refnotify\
 	rawdatad \
 	batterysrv \
 	poweroff_alarm \
--- sp7730ec/base.mk
+++ sp7730ec/base.mk
@@ -68,6 +68,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	nvitemd \
 	charge \
+	refnotify\
 	rawdatad \
 	batterysrv \
 	poweroff_alarm \
