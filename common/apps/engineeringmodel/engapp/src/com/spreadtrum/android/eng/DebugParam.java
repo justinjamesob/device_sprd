683020024958434471f304085965aae1146eda1a
zhangwx <zhangwx@spreadst.com>
2013-09-26 16:33:20 +0800

    bug#221027 <when click the para_set preference,crash >
    [bug number  ]221027
    [root cause  ]NullPointerException
    [changes     ]Change the judge.
    [side effects]
    [reviewers   ]
    
    Change-Id: I75234338195d413187920f5bf5fe994f83f314bf

--- common/apps/engineeringmodel/engapp/src/com/spreadtrum/android/eng/DebugParam.java
+++ common/apps/engineeringmodel/engapp/src/com/spreadtrum/android/eng/DebugParam.java
@@ -149,16 +149,17 @@ public class DebugParam extends PreferenceActivity {
         }
         /*Add 20130523 Spreadst of add RILD  control UI end*/
         /*Add 20130206 Spreadst of 122017 8810 7710 don't support PLMN start*/
-        String mode = SystemProperties.get("ro.product.hardware");
+//        String mode = SystemProperties.get("ro.product.hardware");
         /*Modify 20130306 Spreadst of 130799 77XX dont support PLMN start */
-        if(mode.contains("8810") ||mode.contains("77")||mode.contains("8825")){
-            getPreferenceScreen().removePreference((Preference)findPreference("key_forbidplmn"));
-            getPreferenceScreen().removePreference((Preference)findPreference("key_plmnselect"));
-        }
+//        if(mode.contains("8810") ||mode.contains("77")||mode.contains("8825")){
+//            getPreferenceScreen().removePreference((Preference)findPreference("key_forbidplmn"));
+//            getPreferenceScreen().removePreference((Preference)findPreference("key_plmnselect"));
+//        }
         /*Modify 20130306 Spreadst of 130799 77XX dont support PLMN end  */
         /*Add 20130206 Spreadst of 122017 8810 7710 don't support PLMN end*/
         /*Add 20130718 Spreadst of 181960 modem don't support this AT cmd start*/
         String baseBandVersion = SystemProperties.get("gsm.version.baseband");
+        Log.v("zwx" , "baseBandVersion  = " + baseBandVersion);
         if(baseBandVersion.contains("sc8830")){
             getPreferenceScreen().removePreference((Preference)findPreference("key_forbidplmn"));
             getPreferenceScreen().removePreference((Preference)findPreference("key_plmnselect"));
