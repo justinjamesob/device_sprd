5485c0fb2725ac27e6a51733e1f7981b51a05f31
xin.li <xin.li@spreadtrum.com>
2013-11-15 19:01:16 +0800

    bug#239863 - [Shark] Android4.4 Camera HAL porting
    [bug number ]239863
    [root cause ] not support
    [changes ] support
    [side effects]none
    [reviewers ]baggio.he
    
    Change-Id: I05f42945085efe9dc5523a076f4b69f0856d556d

--- sc8830/device.mk
+++ sc8830/device.mk
@@ -41,8 +41,8 @@ PRODUCT_PACKAGES += \
 	audio.primary.sc8830 \
 	audio_policy.default \
 	gralloc.sc8830 \
+	camera.sc8830 \
 #	hwcomposer.sc8830 \
-#	camera.sc8830 \
 #	lights.sc8830 \
 #	sensors.sc8830
 
