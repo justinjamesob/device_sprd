eab010a2bbff8efa48971fd02f6d9da5370e6b65
chunhou.wang <chunhou.wang@spreadtrum.com>
2013-11-28 17:42:22 -0500

    Bug#240080 Sprdroid4.4_3.10version sp8830ecbase_debug Modem LOG can not useBug #
    [bug number  ]
    [root cause  ]
    [changes     ]
    [side effects]
    [reviewers   ]
    
    Change-Id: I1a53bef13adc62c8e27e18cbbba61e39ed6b05e9

--- scx35_sp8830ec/scx35_sp8830ecbase.mk
+++ scx35_sp8830ec/scx35_sp8830ecbase.mk
@@ -38,7 +38,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.msms.phone_count=1 \
 	persist.msms.phone_count=1 \
 	persist.msms.phone_default=0 \
-	ro.modem.t.count=1
+	ro.modem.t.count=1 \
+        persist.sys.modem.diag=,gser \
+        sys.usb.gser.count=6
 
 # board-specific modules
 PRODUCT_PACKAGES += \
