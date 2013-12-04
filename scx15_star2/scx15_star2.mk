1ba921535673d44aa471387c825449aff2e684a8
Yuming Han <yuming.han@spreadtrum.com>
2013-12-04 13:38:09 +0800

    Bug#244396 Set emergency call in system property
    [bug number  ] 244396
    [root cause  ] missing emergency call list
    [changes     ] add emergency call list
    [side effects] none
    [reviewers   ]

--- scx15_star2/scx15_star2.mk
+++ scx15_star2/scx15_star2.mk
@@ -41,7 +41,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	persist.msmslt=0 \
 	ro.modem.w.count=2 \
 	persist.sys.modem.diag=,gser \
-	sys.usb.gser.count=4
+	sys.usb.gser.count=4 \
+	ro.ril.ecclist=112,911,*911,\#911,110,119,120
 
 # board-specific modules
 #PRODUCT_PACKAGES += \
--- scx35_sp5735ea/scx35_sp5735eabase.mk
+++ scx35_sp5735ea/scx35_sp5735eabase.mk
@@ -38,7 +38,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.msms.phone_count=1 \
 	persist.msms.phone_count=1 \
 	persist.msms.phone_default=0 \
-	ro.modem.w.count=1
+	ro.modem.w.count=1 \
+	ro.ril.ecclist=112,911,*911,\#911,110,119,120
 
 # board-specific modules
 PRODUCT_PACKAGES += \
