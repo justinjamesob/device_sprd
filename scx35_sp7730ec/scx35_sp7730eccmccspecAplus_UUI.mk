38451c08680f40598c0b500f0e05c681e6892fc9
guangping.zhou <guangping.zhou@spreadtrum.com>
2013-12-12 13:19:22 +0800

    Bug #252308<Add 7730EC Board on 4.4_3.10>
    [bug number  ]modify sc8830 flags
    [root cause  ]
    [changes     ]
    [side effects]
    [reviewers   ]
    
    Change-Id: Ic442b62696bd7a148e833d020b4a0d71121642ea

---
+++ scx35_sp7730ec/scx35_sp7730eccmccspecAplus_UUI.mk
@@ -0,0 +1,10 @@
+# export original make file
+LOCAL_ORIGINAL_PRODUCT_MAKEFILE := device/sprd/scx35_sp7730ec/scx35_sp7730ecplus.mk
+
+include $(LOCAL_ORIGINAL_PRODUCT_MAKEFILE)
+# Rename product name and we can lunch it
+PRODUCT_NAME := scx35_sp7730eccmccspecAplus_UUI
+
+$(call inherit-product, vendor/sprd/operator/cmcc/specA.mk)
+$(call inherit-product-if-exists, vendor/sprd/UniverseUI/ThemeRes/universeui.mk)
+$(call inherit-product-if-exists, frameworks/base/data/videos/VideoPackageForUUI.mk)
