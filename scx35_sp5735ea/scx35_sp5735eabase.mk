8bce4b86f6e0ed1408550ba8118b4cf6c7397f6a
JackyLiu <liujk@spreadst.com>
2013-12-13 20:58:40 +0800

    Bug#253336 [new feature] support double SD card, modify default MTP as default MASS_STORAGE
    [Bug Number  ] 253336
    [Root Cause  ] new feature
    [Changes     ] modify default property persist.sys.usb.config as mass_storage
    [Side Effects] no
    [Reviewers   ] chenwei@spreadst.com zhaoyong@spreadst.com
    [Feature ID  ]
    
    Change-Id: Ia260766de5b151ec8d774234d66d560efd7dd727

--- scx35_sp5735ea/scx35_sp5735eabase.mk
+++ scx35_sp5735ea/scx35_sp5735eabase.mk
@@ -30,7 +30,7 @@ PRODUCT_AAPT_CONFIG := hdpi xhdpi
 
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	persist.sys.usb.config=mtp
+	persist.sys.usb.config=mass_storage
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	keyguard.no_require_sim=true \
