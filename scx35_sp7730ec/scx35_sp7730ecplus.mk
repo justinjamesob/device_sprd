17cb3ccd2b40e777d7dfcc222c4bfb2a1fcb8bb7
andrew.zhang@spreadtrum.com <andrew.zhang@spreadtrum.com>
2013-12-31 13:50:26 +0800

    Bug #261734 ro.modem.w.count not set
    [bug number  ] 261734
    [root cause  ] ro.modem.w.count not set
    [changes     ]
    [side effects]
    [reviewers   ]
    
    Change-Id: I31ed90bcd15f6abea4efb6fe8acfc48d81c14589

--- scx35_sp7730ec/scx35_sp7730ecplus.mk
+++ scx35_sp7730ec/scx35_sp7730ecplus.mk
@@ -41,7 +41,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.msms.phone_count=2 \
 	persist.msms.phone_count=2 \
 	persist.msms.phone_default=0 \
-	ro.modem.t.count=2 \
+	ro.modem.w.count=2 \
         persist.sys.modem.diag=,gser \
         sys.usb.gser.count=6
 
