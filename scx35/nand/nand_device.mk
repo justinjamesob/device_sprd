c7c88b4c698aad0d99ac836672b0c2f4dc286317
daniel.li <daniel.li@spreadtrum.com>
2014-01-03 04:50:15 +0800

    Bug #208772 copy nvitemd_td.cfg
    Change-Id: Ida07cc5d541ac61b54d3cee36e1e181a412a8598

--- scx35/nand/nand_device.mk
+++ scx35/nand/nand_device.mk
@@ -1,3 +1,4 @@
 PRODUCT_COPY_FILES += \
 	$(PLATDIR)/nand/fstab.sc8830:root/fstab.sc8830 \
+	$(PLATDIR)/nand/nvitem_td.cfg:system/etc/nvitem_td.cfg \
 	$(PLATDIR)/nand/nvitem_w.cfg:system/etc/nvitem_w.cfg
