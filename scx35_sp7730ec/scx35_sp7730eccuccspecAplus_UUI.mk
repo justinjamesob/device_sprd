1abed9dcd699a39ebce3728b1c500839e15d9363
gaojm <gaojm@spreadst.com>
2014-01-27 17:11:49 +0800

    bug#275798,275818 the default cucc apn should not be modified
    [bug number  ]275798,275818
    [root cause  ]the default cucc apn display error
    [changes     ]Change the build path
    [side effects]no
    [reviewers   ]
    
    Change-Id: Ifeafc771adef563455abab84ec957d47dcf49c89

--- scx35_sp7730ec/scx35_sp7730eccuccspecAplus_UUI.mk
+++ scx35_sp7730ec/scx35_sp7730eccuccspecAplus_UUI.mk
@@ -1,5 +1,7 @@
 -include vendor/sprd/operator/cucc/specA/res/boot/boot_res.mk
 
+$(call inherit-product, vendor/sprd/operator/cucc/specA/res/apn/apn_res.mk)
+
 # export original make file
 LOCAL_ORIGINAL_PRODUCT_MAKEFILE := device/sprd/scx35_sp7730ec/scx35_sp7730ecplus.mk
 
@@ -7,7 +9,7 @@ include $(LOCAL_ORIGINAL_PRODUCT_MAKEFILE)
 # Rename product name and we can lunch it
 PRODUCT_NAME := scx35_sp7730eccuccspecAplus_UUI
 
-$(call inherit-product, vendor/sprd/operator/cucc/specA/res/apn/apn_res.mk)
+$(call inherit-product-if-exists, vendor/sprd/operator/cucc/specA.mk)
 $(call inherit-product-if-exists, vendor/sprd/UniverseUI/ThemeRes/universeui.mk)
 $(call inherit-product-if-exists, frameworks/base/data/videos/VideoPackageForUUI.mk)
 $(call inherit-product, vendor/sprd/partner/shark/bluetooth/device-shark-bt.mk)
