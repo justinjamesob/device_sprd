202aa6ba41a25e054c639a0f708df614deaae021
andyhe <andy.he@spreadtrum.com>
2014-02-13 21:50:02 +0800

    Bug#276965 - [shark pad 4.4][5735][Prime][Sanity]:通话中的界面还有扩音按键,实际无效
    Change-Id: I3be7c122cb357bcb072b26c8b0d0b598d9657658

--- scx35_sp8830ga/scx35_sp8830gabase.mk
+++ scx35_sp8830ga/scx35_sp8830gabase.mk
@@ -57,6 +57,7 @@ PRODUCT_COPY_FILES += \
 	$(BOARDDIR)/audio_params/codec_pga.xml:system/etc/codec_pga.xml \
 	$(BOARDDIR)/audio_params/audio_hw.xml:system/etc/audio_hw.xml \
 	$(BOARDDIR)/audio_params/audio_para:system/etc/audio_para \
+	$(BOARDDIR)/audio_params/audio_policy.conf:system/etc/audio_policy.conf \
 	$(BOARDDIR)/focaltech_ts.idc:system/usr/idc/focaltech_ts.idc \
 	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
 	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
