9a79f626798531ded66a5c25658df689793203dc
shan.cong <shan.cong@spreadtrum.com>
2014-02-18 10:58:34 +0800

    Bug#277626 Add corsica_ve 0.1A board
    Change-Id: I1a25d0abd32201d35a54c9ff876ffd9c3b012af2

--- scx15_corsica_ve/scx15_corsica_ve_0_1_A.mk
+++ scx15_corsica_ve/scx15_corsica_ve_0_1_A.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2011 The Android Open-Source Project
+# Copyright (C) 2007 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,8 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/scx15_corsica_ve.mk \
-	$(LOCAL_DIR)/scx15_corsica_ve_0_1_A.mk
+include device/sprd/scx15_corsica_ve/scx15_corsica_ve.mk
+PRODUCT_NAME := scx15_corsica_ve_0_1_A
+BOARD_TSP_CONFIG := true
 
--- scx15_corsica_ve/vendorsetup.sh
+++ scx15_corsica_ve/vendorsetup.sh
@@ -15,3 +15,4 @@
 #
 
 add_lunch_combo scx15_corsica_ve-userdebug
+add_lunch_combo scx15_corsica_ve_0_1_A-userdebug
