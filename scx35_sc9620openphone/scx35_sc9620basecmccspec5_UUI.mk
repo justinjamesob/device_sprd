905d9fd8b5b4109b16edf981220d90b62ded2c99
jiangyou.li <jiangyou.li@spreadtrum.com>
2014-04-28 21:14:42 +0800

    Bug #306751 Replace the Launcher3 to SprdLauncher1.
    [bug number  ] 306751
    [root cause  ]
    [changes     ]
    [side effects]
    [reviewers   ]
    [self test   ] sprdroid4.4, this problem can't be tested by self.
    
    Change-Id: I61bf967b1466a5ca21790dc42ac419568ec52272

--- scx35_sc9620openphone/scx35_sc9620basecmccspec5_UUI.mk
+++ scx35_sc9620openphone/scx35_sc9620basecmccspec5_UUI.mk
@@ -5,6 +5,9 @@ include $(LOCAL_ORIGINAL_PRODUCT_MAKEFILE)
 # Rename product name and we can lunch it
 PRODUCT_NAME := scx35_sc9620basecmccspec5_UUI
 
+PRODUCT_PACKAGES += \
+    SprdLauncher1
+
 include vendor/sprd/UniverseUI/ThemeRes/universeui.mk
 include vendor/sprd/operator/cmcc/spec5.mk
 
