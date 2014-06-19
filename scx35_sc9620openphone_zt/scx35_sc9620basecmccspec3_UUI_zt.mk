17d4d6a4f2cf8d894a88716c7d336744d8bdd1b8
anita.liu <anita.liu@spreadtrum.com>
2014-06-19 15:18:55 +0800

    Bug#322213 - [sp9620zt] no system default ringtone
    [bug number  ] 322213
    [root cause  ] system default ringtone error
    [changes     ] modify device/sprd/*.mk
    [side effects] none
    [self test   ] reset system, reboot , check notification, ringtone and alarm
    [whether AOB ] no
    [reviewers   ] yuntao.xiao@spreadtrum.com
    
    Change-Id: If3089f26cba1edb781b9fc729113e2d0dd07a440

--- scx35_sc9620openphone_zt/scx35_sc9620basecmccspec3_UUI_zt.mk
+++ scx35_sc9620openphone_zt/scx35_sc9620basecmccspec3_UUI_zt.mk
@@ -5,6 +5,26 @@ include $(LOCAL_ORIGINAL_PRODUCT_MAKEFILE)
 # Rename product name and we can lunch it
 PRODUCT_NAME := scx35_sc9620basecmccspec3_UUI_zt
 
+
+# Set cg prop
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	ro.product.hardware=P1 \
+	ro.product.cg_version=4.4.001.P1.8019 \
+	ro.cg.product.device=Coolpad.8019 \
+	ro.product.releasetime=2014-05-04 \
+	ro.product.model=Coolpad.8019 \
+	ro.product.brand=Coolpad \
+	ro.product.name=Coolpad.8019 \
+	ro.product.device=Coolpad.8019 \
+	ro.product.manufacturer=Coolpad \
+	ro.yulong.version.software=4.4.001.P1.8019 \
+	ro.yulong.version.hardware=P1 \
+	ro.build.display.id=4.4.001.P1.8019 \
+	ro.product.board.customer=cgmobile \
+	ro.config.ringtone=Coolpad.ogg \
+	ro.config.notification_sound=Message01.ogg \
+	ro.config.alarm_alert=Alarm.ogg
+
 PRODUCT_PROPERTY_OVERRIDES += \
         persist.sys.support.vt=false
 
