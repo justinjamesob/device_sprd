092690db720f362c063476896f2dc17e333c0fb0
michael.wang <michael.wang@spreadtrum.com>
2014-07-10 21:21:27 +0800

    Bug #332245 Upload 7731gea specB solutions
    [bug number  ] 332245
    [root cause  ] cucc recommended
    [changes     ] include specB solutions
    [side effects] none
    [self test   ] <MocorDroid4.4_3.10><need add new product and make>
    [whether AOB ] No
    [reviewers   ] yuntao.xiao,huibin.mao
    
    Change-Id: Idbcb369044a44fcb75cc5ff18ab6838ad45cc425

--- scx35_sp7731gea/AndroidProducts.mk
+++ scx35_sp7731gea/AndroidProducts.mk
@@ -27,5 +27,6 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/scx35_sp7731geaplus_dt.mk \
 	$(LOCAL_DIR)/scx35_sp7731geacuccspecAplus_UUI.mk \
 	$(LOCAL_DIR)/scx35_sp7731geacuccspecAplus_UUI_dt.mk \
-	$(LOCAL_DIR)/scx35_sp7731geaplus_UUI_dt.mk
+	$(LOCAL_DIR)/scx35_sp7731geaplus_UUI_dt.mk \
+	$(LOCAL_DIR)/scx35_sp7731geacuccspecBplus_UUI_dt.mk
 	
