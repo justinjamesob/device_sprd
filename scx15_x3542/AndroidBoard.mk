cbe23a4adc058fc4c6799beddc93c4a424aff8cc
Wuling.Zheng <Wuling.Zheng@spreadtrum.com>
2014-08-06 16:48:12 +0800

    Bug #340624 remove modem bins files to fix target files pack
    [bug number  ] 340624
    [root cause  ]
    [changes     ]
    [side effects]
    [self test   ] yes
    [whether AOB ]
    [reviewers   ]
    
    Change-Id: I144765502070ab97715f17d68f19f974c5f69eba

--- scx15_x3542/AndroidBoard.mk
+++ scx15_x3542/AndroidBoard.mk
@@ -1,10 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
-$(call add-radio-file,modem_bins/wmodem.bin)
-$(call add-radio-file,modem_bins/wnvitem.bin)
-$(call add-radio-file,modem_bins/wdsp.bin)
-$(call add-radio-file,modem_bins/wcnnvitem.bin)
-$(call add-radio-file,modem_bins/wcnmodem.bin)
+#remove modem-bins files to fix target-files-package
+#$(call add-radio-file,modem_bins/wmodem.bin)
+#$(call add-radio-file,modem_bins/wnvitem.bin)
+#$(call add-radio-file,modem_bins/wdsp.bin)
+#$(call add-radio-file,modem_bins/wcnnvitem.bin)
+#$(call add-radio-file,modem_bins/wcnmodem.bin)
 
 # Compile U-Boot
 ifneq ($(strip $(TARGET_NO_BOOTLOADER)),true)
