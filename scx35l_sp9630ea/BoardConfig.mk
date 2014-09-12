844c1feaac928071f565df31a1b095f2f771b72d
jackson.sun <jackson.sun@spreadtrum.com>
2014-09-12 15:00:31 +0800

    Bug #344271 -- Load driver on bootup, and do not unload driver when turn off wifi
    [bug number  ] 344271
    [root cause  ] Wifi driver can not alloc enough memory when system running a while
    [changes     ] Load driver on bootup, and do not unload driver when turn off wifi
    [side effects] Load driver on bootup, and do not unload driver when turn off wifi
    [self test   ] 7731
    [whether AOB ] No
    [reviewers   ] Enlai.Chu
    
    Change-Id: Ie06c48c1d4677bb53c26eba7f3e717c01851229a

--- scx35l_sp9630ea/BoardConfig.mk
+++ scx35l_sp9630ea/BoardConfig.mk
@@ -106,7 +106,6 @@ WIFI_DRIVER_FW_PATH_PARAM   :="sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "system/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_P2P     := "system/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_AP      := "system/vendor/firmware/fw_bcmdhd_apsta.bin"
-WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcmdhd.ko"
 WIFI_DRIVER_MODULE_ARG      := "firmware_path=system/vendor/firmware/fw_bcmdhd.bin nvram_path=system/etc/wifi/bcmdhd.cal"
 WIFI_DRIVER_MODULE_NAME     := "bcmdhd"
 endif
