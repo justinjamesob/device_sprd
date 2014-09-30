30fd78b98910d1169ab1a63eb5670eb095a881d1
xin.li <xin.li@spreadtrum.com>
2014-09-30 17:59:10 +0800

    Bug #356725 fix cts testCameraFeatures fail
    [bug number ]356725
    [root cause ]9630's makefile not config camera features
    [changes ]add config
    [side effects]NO
    [reviewers ]jianping.wang
    [self test ]9630
    
    Change-Id: I5a9d49536d58173ad64e2e5157529f76dfad24ff

--- scx35l_sp9630ea/scx35l_sp9630ea_dt_plus.mk
+++ scx35l_sp9630ea/scx35l_sp9630ea_dt_plus.mk
@@ -82,6 +82,7 @@ PRODUCT_COPY_FILES += \
 	$(BOARDDIR)/audio_params/audio_para:system/etc/audio_para \
 	$(BOARDDIR)/audio_params/audio_policy.conf:system/etc/audio_policy.conf \
 	$(BOARDDIR)/focaltech_ts.idc:system/usr/idc/focaltech_ts.idc \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
 	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
 	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
 	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
