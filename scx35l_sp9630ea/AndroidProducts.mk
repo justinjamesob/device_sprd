c1ab2c8529142c7e24ee960ede52a5318f856664
yuntao.xiao <yuntao.xiao@spreadtrum.com>
2015-01-29 20:16:21 +0800

    Bug #400356 Add non CMCC plus product for SharkL
    [root cause  ]The CTS need this version
    [changes     ]Add the board configs with non-operator
    [side effects]nil
    [self test   ]<9630ea>:lunch scx35l_sp9630ea_dt_plus_UUI or scx35l_sp9630ea_dt_plus_UUI_1350MHZ can pass
    [whether AOB ]false
    [reviewers   ]
    
    Change-Id: I1814d236444f4e614f3d390ffaa4035e9121658a

--- scx35l_sp9630ea/AndroidProducts.mk
+++ scx35l_sp9630ea/AndroidProducts.mk
@@ -21,8 +21,10 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/scx35l_sp9630ea_dt_plus.mk \
 	$(LOCAL_DIR)/scx35l_sp9630ea_dt_base_cmccspec3_UUI.mk \
 	$(LOCAL_DIR)/scx35l_sp9630ea_dt_plus_cmccspec3_UUI.mk \
+	$(LOCAL_DIR)/scx35l_sp9630ea_dt_plus_UUI.mk \
 	$(LOCAL_DIR)/scx35l_sp9630ea_dt_base_UUI.mk \
 	$(LOCAL_DIR)/scx35l_sp9630ea_dt_plus_cmccspec3_UUI_nowcn.mk \
 	$(LOCAL_DIR)/scx35l_sp9630ea_dt_plus_cmccspec3_UUI_1350MHZ.mk \
+	$(LOCAL_DIR)/scx35l_sp9630ea_dt_plus_UUI_1350MHZ.mk \
   $(LOCAL_DIR)/scx35l_sp9630ea_dt_base_UUI.mk
 
